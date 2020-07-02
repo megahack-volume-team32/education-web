d572793c78f1aee3c908aa696c831a2ce4f5e5b6
linha9 <linha9theawesome@gmail.com>
2020-07-02 16:04:26 -0300

    Fix Login Page Style

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./LoginPage.scss";
-import "./GlobalStyles.css";
 
 import { Button, Card, Form } from "react-bootstrap";
 
 function LoginPage() {
   return (
-    <div className="Container FlexCenterColumn Background">
+    <div className="LoginScreen">
       <div className="LoginForm">
-        <div className="LoginTitleContainer">
+        <div>
           <h1>Login</h1>
         </div>
-        <div className="TextBox">
+        <div className="LoginInput">
           <input type="text" placeholder="Username" />
         </div>
 
-        <div className="TextBox">
+        <div className="LoginInput">
           <input type="password" placeholder="Password" />
         </div>
 
